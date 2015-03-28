ba60cb69bda3a64c2c8c26ffb5563ca79212655a
Yang Zi Dong <dong_yang_zi@ida.gov.sg>
2015-03-28 17:00:38 +0800

    Added test file that contains code for entering credit card information

--- backend/donate.rb
+++ backend/donate.rb
@@ -6,8 +6,17 @@ Braintree::Configuration.merchant_id = "thp37yf88fy592rp"
 Braintree::Configuration.public_key = "s9748gsdp7hr24nh"
 Braintree::Configuration.private_key = "c93830fab264bbd03285dee81a9e23e6"
 
-get "/donate/" do
+get "/test" do
+  File.read('client_test.html')
+end
+
+get "/account" do
   @client_token = Braintree::ClientToken.generate
   content_type :json
   {:client_token => @client_token, :client_name => 'Tan Kah Kee' }.to_json
 end 
+
+post "/donations" do
+  nonce = params[:payment_method_nonce]
+  puts 'Received payment: ' + nonce
+end
