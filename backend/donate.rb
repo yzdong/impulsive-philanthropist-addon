d7b27ba31d2745a6cca9da57fb0f4b325a27976a
Yang Zi Dong <dong_yang_zi@ida.gov.sg>
2015-03-28 20:54:54 +0800

    Changed UI of popup

--- backend/donate.rb
+++ backend/donate.rb
@@ -10,29 +10,6 @@ Braintree::Configuration.merchant_id = "thp37yf88fy592rp"
 Braintree::Configuration.public_key = "s9748gsdp7hr24nh"
 Braintree::Configuration.private_key = "c93830fab264bbd03285dee81a9e23e6"
 
-# get "/test" do
-#   File.read('client_test.html')
-# end
-
-# get "/account" do
-#   @client_token = Braintree::ClientToken.generate
-#   content_type :json
-#   {:client_token => @client_token, :client_name => 'Tan Kah Kee' }.to_json
-# end 
-
-
-# post "/donations" do
-#   nonce = params[:payment_method_nonce]
-#   result = Braintree::Transaction.sale(
-#     :amount => "10.00",
-#     :payment_method_nonce => nonce, 
-#     :options => {
-#       :submit_for_settlement => true
-#     }
-#   )
-#   puts 'Received payment: ' + nonce
-# end
-
 post "/payment" do
   result = Braintree::Transaction.sale(
     :amount => "10.00",
@@ -46,6 +23,12 @@ post "/payment" do
       :submit_for_settlement => true
     }
   )
+  message = 'Transaction failed, try again'
+  if result.success? 
+    message = 'Thank you for your donation!'
+  end
+  content_type :json
+  {message: message}.to_json
 end
 
 get '/charity/all/' do
