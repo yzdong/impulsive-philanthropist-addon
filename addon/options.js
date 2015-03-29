21463cbe7c2dbf548c24bdee599b73aea4ba54bb
Yun Long <chong.yunlong@gmail.com>
2015-03-29 10:23:46 +0800

    added storage of preferences fixed alignment of cards

--- addon/options.js
+++ addon/options.js
@@ -2,8 +2,43 @@
 $(function() {
 //   // Handler for .ready() called.
 	$( ".polaroid" ).click(function() {
-		console.log('lol');
+		// console.log('lol');
 	  $(this).toggleClass("active");
+	  var categories = [];
+	  var cat = $(this).find('p').text();
+	  if ($(this).hasClass("active")){
+	  	
+	  	
+	  	console.log($(this).find('p').text())
+	  	
+	  		chrome.storage.sync.get('categories', function(result){
+	  			console.log('lol' + result);
+	  			if (result.value === undefined) {
+	  				categories.push(cat);
+	  			}
+	  			else{
+	  				categories = result.value;
+	  				categories.push(cat);
+	  			}
+
+	  		});
+          chrome.storage.sync.set({'categories': categories}, function() {
+          	console.log("saved");
+        	});
+	  } else {
+	  	chrome.storage.sync.get('categories', function(result){
+	  		if (!(result.value === undefined)) {
+	  			categories = result.value;
+	  			categories.splice(categories.indexOf(cat),1);
+	  		}
+	  		
+	  	});
+	  	chrome.storage.sync.set({'categories': categories}, function() {
+          		console.log("saved deleted");
+        });
+
+	  }
+
 	});
 
 	
